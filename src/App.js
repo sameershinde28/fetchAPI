f8719bf28d85e5fe1b141a366dad7e474bcf5fad
sameer <sameershinde@gmail.com>
2023-05-03 18:56:01 +0530

    initial commit fetch API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useEffect,useState} from 'react'
 function App() {
+  const [users,setUser]=useState([])
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos").then((result)=>{
+      result.json().then((resp)=>{
+        // console.warn(resp)
+        setUser(resp)
+      })
+    })
+  },[])
+  console.warn(users)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get API Call </h1>
+      <table border="1">
+       <tbody>
+       <tr>
+          <td>UserId</td>
+          <td>Id</td>
+          <td>Title</td>
+          <td>Completed</td>
+        </tr>
+        {
+          users.map((item,i)=>
+            <tr key={i}>
+            <td>{item.userId}</td>
+          <td>{item.id}</td>
+          <td>{item.title}</td>
+            <td>{item.completed}</td>  
+          </tr>
+          )
+        }
+       </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
