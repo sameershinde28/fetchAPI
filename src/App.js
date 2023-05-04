befa94b9a42048432af7810ee87c273089d6746a
sameer <sameershinde@gmail.com>
2023-05-04 16:53:13 +0530

    push api initial commit video 53

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import './App.css';
-import React,{useEffect,useState} from 'react'
+import React, {useState } from 'react'
 function App() {
-  const [users,setUser]=useState([])
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/todos").then((result)=>{
-      result.json().then((resp)=>{
-        // console.warn(resp)
-        setUser(resp)
-      })
-    })
-  },[])
-  console.warn(users)
+ const [name,setName]=useState("");
+ const [email,setEmail]=useState("");
+ const [mobile,setMobile]=useState("");
+function saveData()
+{
+  let data={name,email,mobile}
+console.warn(data);
+// console.warn(name,email,mobile);
+
+}
+    
   return (
     <div className="App">
-      <h1>Get API Call </h1>
-      <table border="1">
-       <tbody>
-       <tr>
-          <td>UserId</td>
-          <td>Id</td>
-          <td>Title</td>
-          <td>Completed</td>
-        </tr>
-        {
-          users.map((item,i)=>
-            <tr key={i}>
-            <td>{item.userId}</td>
-          <td>{item.id}</td>
-          <td>{item.title}</td>
-            <td>{item.completed}</td>  
-          </tr>
-          )
-        }
-       </tbody>
-      </table>
+      <h1>POST API Example </h1>  
+      <input type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}}  /> <br /> <br />
+      <input type="text" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} /> <br /> <br />
+      <input type="text" name="mobile"  value={mobile} onChange={(e)=>{setMobile(e.target.value)}}/> <br /> <br />
+      <button type="button" onClick={saveData} >Save New User</button>
     </div>
   );
 }
